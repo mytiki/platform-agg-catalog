e7bc7585e811093512a19014d2eca33990d7c837
Mike Audi <mike@mytiki.com>
2023-08-24 12:31:37 -0500

    feat: setup basic function

--- src/main/java/com/mytiki/ocean/catalog/App.java
+++ src/main/java/com/mytiki/ocean/catalog/App.java
@@ -7,14 +7,15 @@ package com.mytiki.ocean.catalog;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
-import com.amazonaws.services.lambda.runtime.events.SQSEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPResponse;
 
 
-public class App implements RequestHandler<SQSEvent, Void> {
-    public Void handleRequest(final SQSEvent event, final Context context) {
-        for(SQSEvent.SQSMessage msg : event.getRecords()){
-            System.out.println(new String(msg.getBody()));
-        }
-        return null;
+public class App implements RequestHandler<APIGatewayV2HTTPEvent, APIGatewayV2HTTPResponse> {
+    public APIGatewayV2HTTPResponse handleRequest(final APIGatewayV2HTTPEvent request, final Context context) {
+        return APIGatewayV2HTTPResponse.builder()
+                .withStatusCode(200)
+                .withBody(request.getBody())
+                .build();
     }
 }
--- src/test/java/com/mytiki/ocean/catalog/AppTest.java
+++ src/test/java/com/mytiki/ocean/catalog/AppTest.java
@@ -5,21 +5,25 @@
 
 package com.mytiki.ocean.catalog;
 
-import com.amazonaws.services.lambda.runtime.events.SQSEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPResponse;
 import org.junit.Test;
 
-import java.util.List;
+import java.util.UUID;
+
+import static org.junit.Assert.assertEquals;
 
 public class AppTest {
 
     @Test
     public void success() {
         App app = new App();
-        SQSEvent.SQSMessage message = new SQSEvent.SQSMessage();
-        message.setBody("hello");
-        List<SQSEvent.SQSMessage> messages = List.of(message);
-        SQSEvent event = new SQSEvent();
-        event.setRecords(messages);
-        app.handleRequest(event, null);
+        String body = UUID.randomUUID().toString();
+        APIGatewayV2HTTPEvent request = APIGatewayV2HTTPEvent.builder()
+                .withBody(body)
+                .build();
+        APIGatewayV2HTTPResponse response = app.handleRequest(request, null);
+        assertEquals(200, response.getStatusCode());
+        assertEquals(body, response.getBody());
     }
 }
