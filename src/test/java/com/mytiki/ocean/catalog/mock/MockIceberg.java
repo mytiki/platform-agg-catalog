e475757c076d4223c26b5336090d100d064a3655
Mike Audi <mike@mytiki.com>
2023-08-25 13:00:27 -0500

    fix: make the stubbing in the iceberg helper lenient

--- src/test/java/com/mytiki/ocean/catalog/mock/MockIceberg.java
+++ src/test/java/com/mytiki/ocean/catalog/mock/MockIceberg.java
@@ -33,20 +33,20 @@ public class MockIceberg {
             Schema schema = Mockito.mock(Schema.class);
             location = prop.getProperty("warehouse");
             name = prop.getProperty("database-name");
-            Mockito.doReturn(location).when(iceberg).getWarehouse();
-            Mockito.doReturn(Namespace.of(name)).when(iceberg).getDatabase();
-            Mockito.doNothing().when(iceberg).close();
-            Mockito.doNothing().when(iceberg).renameTable(Mockito.any(), Mockito.any());
-            Mockito.doReturn(this.schema).when(schema).toString();
-            Mockito.doReturn(List.of()).when(spec).fields();
-            Mockito.doReturn(schema).when(table).schema();
-            Mockito.doReturn(spec).when(table).spec();
-            Mockito.doReturn(location).when(table).location();
-            Mockito.doReturn(name).when(table).name();
-            Mockito.doReturn(table).when(iceberg).loadTable(Mockito.any());
-            Mockito.doReturn(table).when(iceberg)
+            Mockito.lenient().doReturn(location).when(iceberg).getWarehouse();
+            Mockito.lenient().doReturn(Namespace.of(name)).when(iceberg).getDatabase();
+            Mockito.lenient().doNothing().when(iceberg).close();
+            Mockito.lenient().doNothing().when(iceberg).renameTable(Mockito.any(), Mockito.any());
+            Mockito.lenient().doReturn(this.schema).when(schema).toString();
+            Mockito.lenient().doReturn(List.of()).when(spec).fields();
+            Mockito.lenient().doReturn(schema).when(table).schema();
+            Mockito.lenient().doReturn(spec).when(table).spec();
+            Mockito.lenient().doReturn(location).when(table).location();
+            Mockito.lenient().doReturn(name).when(table).name();
+            Mockito.lenient().doReturn(table).when(iceberg).loadTable(Mockito.any());
+            Mockito.lenient().doReturn(table).when(iceberg)
                     .createTable(Mockito.any(), Mockito.any(), Mockito.any(), Mockito.any(), Mockito.any());
-            Mockito.doReturn(false).when(iceberg).tableExists(Mockito.any());
+            Mockito.lenient().doReturn(false).when(iceberg).tableExists(Mockito.any());
         } catch (IOException ex) {
             throw new RuntimeException(ex);
         }
